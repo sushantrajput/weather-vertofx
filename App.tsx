fec707c28277b7eb3082f48ec4cf9edb040d8d71
Sushant Singh Rajput <skrajsushant698@gmail.com>
2025-12-21 14:08:26 +0530

    analytics added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/next"
 import React, { useState } from 'react';
 import Header from './components/Header';
 import ErrorModal from './components/ErrorModal';
